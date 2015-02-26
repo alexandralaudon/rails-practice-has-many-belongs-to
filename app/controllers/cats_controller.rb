7d9e2d746c3fe30af3fc573bd2192882ca1f6d58
Alex <alexandralaudon@gmail.com>
2015-02-26 13:20:45 -0700

    Completed exercise

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -2,11 +2,11 @@ class CatsController < ApplicationController
 
   def index
     @cats = Cat.all
-    @toys = @cat.toys
   end
 
   def show
     @cat = Cat.find(params[:id])
+    @toys = @cat.toys
   end
 
 end
